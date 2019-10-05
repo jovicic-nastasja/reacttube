b874e7298f08ba3dcca4b6850eabb23398fff7b1
Nastasja Jovicic <jovicic.nastasja@gmail.com>
2019-10-05 17:43:23 +0300

    added build

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-## Application details
+## Aplication details
 
-Application was created with ReactJS and YouTube API.
+Aplication was created with ReactJS and YouTube API.
 
-### Application functions
+### Aplication functions
 
-The application lists the last 10 videos that have been uploaded to a channel.
+The aplication lists the last 10 videos that have been uploaded to a channel.
 
-Live version: [link](http://nastasja-jovicic.fhio.xyz/projects/youtube/) to view it in the browser.
+Live demo: 
---
+++ build/precache-manifest.e42904385b7835a1a3fe5a3ebeae2799.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b629bdea88ab0cbcacdc",
+    "url": "/static/js/main.b629bdea.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "b629bdea88ab0cbcacdc",
+    "url": "/static/css/main.47b879c2.chunk.css"
+  },
+  {
+    "revision": "b8ccc746939f0af58c2e5d1be69e5895",
+    "url": "/index.html"
+  }
+];
